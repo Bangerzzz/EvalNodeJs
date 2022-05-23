dc17bd11cdd2528d859aada505f477f26b7bd1c2
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-05-23 15:22:07 +0200

    correction erreur Joi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 const bodyParser = require('body-parser');
 require('dotenv/config');
 
