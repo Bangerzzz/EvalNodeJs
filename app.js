eea2c8e259070e2f7d35f5ccf6dd81f250475440
Bangerzzz <kaci-chaouche.omar@hotmail.com>
2022-05-23 16:00:30 +0200

    Connexion

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ app.use('/posts',postsRoute);
 app.get('/',(req,res) => {
 	res.send('Home page');
 });
- 
+//Inscription
 app.post("/signup", async (req, res) => {
     const payload = req.body;
     const schema = Joi.object({
-      email:Joi.string().min(3).max(50).required(),
+      email:Joi.string().min(3).max(50).required().email(),
       username: Joi.string().min(3).max(50).required(),
       password: Joi.string().min(3).max(50).required(),
     });
@@ -39,7 +39,38 @@ app.post("/signup", async (req, res) => {
         name: account.name,
       });
     }
+  Accounts.insertOne(account);
+  res.status(201).json({
+    name: account.name,
+    email: account.email,
   });
+});
+
+// CONNEXION
+app.post("/signin", async (req, res) => {
+  const payload = req.body;
+  const schema = Joi.object({
+    email: Joi.string().max(255).required().email(),
+    password: Joi.string().min(3).max(50).required(),
+  });
+
+  const { value: connexion, error } = schema.validate(payload);
+  if (error) return res.status(400).send({ erreur: error.details[0].message });
+
+  const { id, found: account } = Accounts.findByProperty(
+    "email",
+    connexion.email
+  );
+  if (!account) return res.status(400).send({ erreur: "Email Invalide" });
+
+  const passwordIsValid = await bcrypt.compare(req.body.password, account.password);
+  if (!passwordIsValid)
+    return res.status(400).send({ erreur: "Mot de Passe Invalide" });
+
+  const token = jwt.sign({ id }, process.env.JWT_PRIVATE_KEY);
+  res.header("x-auth-token", token).status(200).send({ name: account.name });
+});
+
 
 mongoose.connect(process.env.DB_CONNECTION, () => console.log('connected to DB!'));
 
